8fa4ad6551d5f6f8d88440d7bfd50c7273afa0db
Bibek <jujulalshrestha@gmail.com>
2016-02-01 08:09:36 -0500

    Final Submit

--- src/Lesson9/Prob4/Squares.java
+++ src/Lesson9/Prob4/Squares.java
@@ -6,18 +6,23 @@ import java.util.function.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import Lesson9.Prob3.Employee;
+
 public class Squares {
-	//Function<int> squar = n->Math.sqrt(n);
+	static Function<Integer,Double> mySquare = n->Math.pow(n,2);
+	//static Function<Integer, Double> mySquare = e -> Math.sqrt(e);
 	
 	public static void printSquare(int num){
-		IntStream squarList =IntStream.iterate(1,n->n+1)
-			.filter(n->Math.sqrt(n).map(x->Double::new).intValue().limit(num));
-			
+		List<Integer> squarList =IntStream.iterate(1,n->n+1).limit(num)
+				.map(x->(int)Math.pow(x,2))
+				.boxed()
+				.collect(Collectors.toList());
+		System.out.println(squarList);
 	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		printSquare(15);
 	}
 
 }
